1fbe20cfb3280c95782593168e9513c08e350157
William Sharp <wsharp07@users.noreply.github.com>
2021-01-03 18:10:49 -0500

    Updating dependencies (#2)
    * updating deps
    
    * upgrading react native elements and changing code

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Header, Divider } from 'react-native-elements';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Temperature from './components/temperature/Temperature';
 import AppInfo from './components/appinfo/AppInfo';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header
-        leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'NORTH CORE', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
-      />
-      <Divider style={styles.divider} />
-      <Temperature></Temperature>
-      <Divider style={styles.divider} />
-      <AppInfo></AppInfo>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'NORTH CORE', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+        <Divider style={styles.divider} />
+        <Temperature></Temperature>
+        <Divider style={styles.divider} />
+        <AppInfo></AppInfo>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
